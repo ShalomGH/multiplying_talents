07bcf9a5af6316ae2ff9bed25fbdba54b22bb444
ShalomGH <ShalomGH@mail.com>
2021-12-26 22:02:44 +0600

    fix start on windows

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ import sys
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import QCoreApplication, Qt, QUrl
 
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
+
     engine = QQmlApplicationEngine()
-    engine.load(os.fspath(Path(__file__).resolve().parent / "UI/main.qml"))
-    if not engine.rootObjects():
-        sys.exit(-1)
+
+    filename = os.fspath("UI/main.qml")
+    url = QUrl.fromLocalFile(filename)
+
+
+    def handle_object_created(obj, obj_url):
+        if obj is None and url == obj_url:
+            QCoreApplication.exit(-1)
+
+
+    engine.objectCreated.connect(handle_object_created, Qt.QueuedConnection)
+    engine.load(url)
+
     sys.exit(app.exec())
